5e5aa8938c21842ec37d2f66c9d4eb66752ff632
hyobinkim <hyobim0911@gmail.com>
2022-07-17 15:20:33 +0900

    Init:#1 - init project
    초기 프로젝트 설정 아직 패키지 구조 적용 안함

---
+++ proso/src/main/java/com/prosoteam/proso/ProsoApplication.java
@@ -0,0 +1,13 @@
+package com.prosoteam.proso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsoApplication.class, args);
+	}
+
+}
---
+++ proso/src/test/java/com/prosoteam/proso/ProsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosoteam.proso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
