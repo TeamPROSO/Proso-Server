6292814b87d1bf5c0b844c3bbc358a930acdae88
hyobinkim <hyobim0911@gmail.com>
2022-07-17 17:01:30 +0900

    Feat:#3 - add user Entity
    카카오 회원 가입을 위한 유저 엔티티 추가

---
+++ proso/src/main/java/com/prosoteam/proso/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.prosoteam.proso.domain.user.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_TB")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_IDX")
+    private Long userIdx;
+
+    @Column(nullable = false, unique = true,name = "USERNAME")
+    private String  username;
+
+    @Column(nullable = false,length =50,name= "NICKNAME")
+    private String nickname;
+
+    @Column(nullable = false, length = 100,name = "PASSWORD")
+    private String password;
+
+    @Column(name = "USER_Img")
+    private String userImg ;
+}
