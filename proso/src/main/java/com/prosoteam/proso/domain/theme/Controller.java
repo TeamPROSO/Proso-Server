8570841be7412911f39668c203d4af76cb8c0cda
yeominyoung <dufmasky99@naver.com>
2022-08-03 15:41:01 +0900

    Fix:#4 - CommonResponse 추가
    CommonResponse 추가하여 구조 통일

--- proso/src/main/java/com/prosoteam/proso/domain/theme/Controller.java
+++ proso/src/main/java/com/prosoteam/proso/domain/theme/Controller.java
@@ -2,9 +2,11 @@ package com.prosoteam.proso.domain.theme;
 
 import com.prosoteam.proso.domain.theme.model.Theme;
 import com.prosoteam.proso.domain.theme.model.ThemeCreationRequest;
+import com.prosoteam.proso.global.common.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.prosoteam.proso.global.common.CommonResponse;
 
 @RestController
 @RequestMapping(value = "/api/themes")
@@ -14,32 +16,39 @@ public class Controller {
 
     //테마 생성
     @PostMapping("/theme")
-    public ResponseEntity<Theme> createTheme (@RequestBody ThemeCreationRequest request) {
-        return ResponseEntity.ok(themeService.createTheme(request));
+    public CommonResponse<Theme> createTheme (@RequestBody ThemeCreationRequest request) {
+        return CommonResponse.success(themeService.createTheme(request));
     }
 
     //테마 조회(아이디로 테마조회 or 테마 전체 조회 둘 다 가능)
     @GetMapping("/theme")
-    public ResponseEntity readThemes(@RequestParam(required = false) Long themeId) {
+    public CommonResponse readThemes(@RequestParam(required = false) Long themeId) {
         if (themeId == null) {
-            return ResponseEntity.ok(themeService.readThemes());
+            return CommonResponse.success(themeService.readThemes());
         }
-        return ResponseEntity.ok(themeService.readTheme(themeId));
+        return CommonResponse.success(themeService.readTheme(themeId));
     }
 
     //테마 수정
     @PatchMapping("/theme/{themeId}")
-    public ResponseEntity<Theme> updateTheme(@RequestBody ThemeCreationRequest request, @PathVariable Long themeId){
-        return ResponseEntity.ok(themeService.updateTheme(themeId, request));
+    public CommonResponse<Theme> updateTheme(@RequestBody ThemeCreationRequest request, @PathVariable Long themeId){
+        return CommonResponse.success(themeService.updateTheme(themeId, request));
     }
 
-    //테마 삭제
+
     @DeleteMapping("/theme/{themeId}")
-    public ResponseEntity<Void> deleteTheme (@PathVariable Long themeId) {
-        themeService.deleteTheme(themeId);
-        return ResponseEntity.ok().build();
+    public CommonResponse<String> deleteTheme(@PathVariable Long themeId){
+        try{
+            themeService.deleteTheme(themeId);
+            String result = "테마가 성공적으로 삭제되었습니다.";
+            return CommonResponse.success(result);
 
+        }catch(BaseException exception){
+
+        }
+        return null;
     }
+
 
 
 }
